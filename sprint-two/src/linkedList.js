6d95cb1f91dd839ebba5626792316614b7e80733
beasta <barryblaha@gmail.com>
2015-08-20 22:24:03 -0700

    working on Tree now

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,13 +3,54 @@ var LinkedList = function(){
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value){
+  list.addToTail = function(value){ 
+  //needs to: add pointer from old tail to new tail
+  //needs to : create new node
+    var tempNode = new Node(value);
+    
+
+    if(list.head === null){
+      tempNode.next = list.head;
+      list.head = tempNode;
+      list.tail = list.head;
+
+    }else{
+      list.tail.next = tempNode;
+      list.tail = tempNode; 
+    }
+
   };
 
   list.removeHead = function(){
+    //list.head = list.head;
+    var temp = list.head;
+    list.head = list.head.next;
+    return temp.value;
+    delete temp;
   };
 
   list.contains = function(target){
+    var check = list.head;
+
+
+    //console.dir(check.next);
+    while (check.next!==null){
+
+      if(check.value === target){
+        //console.log('should return true');
+        return true;
+      }
+
+      check = check.next;
+    }
+
+    if(check.value === target){
+        //console.log('should return true');
+        return true;
+    }
+
+    //console.log('should return false');
+    return false;
   };
 
   return list;
@@ -27,3 +68,4 @@ var Node = function(value){
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+//list.addToTail(new Node('cool'))  
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,25 +3,35 @@ var Tree = function(value){
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
+
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
 
   // your code here
-  newTree.children = null;  // fix me
+  //newTree.children = null;  // fix me
 
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  
+
+  if(this.top === null){
+    newTree[value] = value;
+    this.top 
+  }
 
 };
 
 treeMethods.contains = function(target){
 
 };
+
+
 
 
 /*
