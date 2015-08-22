deb00899515a0d989d67e383a79b49a1b41af22b
beasta <barryblaha@gmail.com>
2015-08-21 21:11:41 -0700

    hashTable.js is COMPLETE

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -12,15 +12,15 @@ HashTable.prototype.insert = function(k, v){ //key parameter, value parameter
   if(!bucket){ // if if there is nothing in bucket, create an empty array to push to
     bucket = [];
   }else{
-    for (var i = 0; i < bucket.length; i++) {
-      if(bucket[i][0]===k){
+    for (var j = 0; j < bucket.length; j++) {
+      if(bucket[j][0]===k){
         duplicate=true;
-        bucket[i][1]=v;
+        bucket[j][1]=v;
       };
     };  
   }
-  console.log("k:"+k+", v:"+v);
-  console.dir(bucket)
+  //console.log("k:"+k+", v:"+v);
+  //console.dir(bucket)
   if(!duplicate){
     bucket.push([k,v]);
   }
@@ -31,9 +31,9 @@ HashTable.prototype.insert = function(k, v){ //key parameter, value parameter
 HashTable.prototype.retrieve = function(k){  //key parameter, returns value
   var i = getIndexBelowMaxForKey(k, this._limit);
   var bucket = this._storage.get(i);
-  for (var i = 0; i < bucket.length; i++) {
-    if(bucket[i][0]===k){
-      return bucket[i][1];
+  for (var j = 0; j < bucket.length; j++) {
+    if(bucket[j][0]===k){
+      return bucket[j][1];
     };
   };
   
@@ -41,14 +41,14 @@ HashTable.prototype.retrieve = function(k){  //key parameter, returns value
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  /*var bucket = this._storage.get(i);
-  for (var i = 0; i < bucket.length; i++) {
-    if(bucket[i][0]===k){
-      this._storage.set(i,bucket[i])
+  var bucket = this._storage.get(i);
+  for (var j = 0; j < bucket.length; j++) {
+    if(bucket[j][0]===k){
+      bucket[j][1]=null;
     };
   };
-*/
-  this._storage.set(i,null);
+
+  this._storage.set(i,bucket);
 };
 
 
