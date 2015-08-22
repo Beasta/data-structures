826cb5bc68d703f177c493925c7aef5b8b0580e4
beasta <barryblaha@gmail.com>
2015-08-21 19:54:12 -0700

    working on collision in hashTable now

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,15 +5,42 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var thisRetrieval = this.retrieve(k);
+  if(!thisRetrieval){
+    this._storage.set(i,[]);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  thisRetrieval.push(this._storage.set(i,v));
+  this._storage.set(i,thisRetrieval);
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  /*return*/ var bucket = this._storage.get(i);
+  for (var i = 0; i < buck
 
-};
+
+
+
+
+
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(i,null);
 };
 
 
