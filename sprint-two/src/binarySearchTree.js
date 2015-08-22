fe7cd460c4fe77526c25859934488a5d59e3ef2b
beasta <barryblaha@gmail.com>
2015-08-21 22:19:01 -0700

    setup template

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,44 @@
 var BinarySearchTree = function(value){
+  var newTree = Object.create(treeMethods);
+  newTree.left = null;
+  newTree.right = null;
+  newTree.value = value;
+  newTree.children = [];
+
+  return newTree;
 };
+
+
+
+
+
+
+var treeMethods = {};
+
+treeMethods.insert = function(value){
+  if()
+
+};
+
+treeMethods.contains = function(value){
+
+
+  return;//returns a boolean
+};
+
+treeMethods.depthFirstLog = function(func){
+
+};
+
+
+
+
+
+
+
+
+
+
 
 
 /*
